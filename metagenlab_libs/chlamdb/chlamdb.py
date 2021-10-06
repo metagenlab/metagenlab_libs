c4c1a670737dbb5060193dd7178ff980b9451c9d
bastian <bastian.marquis@chuv.ch>
2021-10-06 11:05:35 +0200

    No header in the cog-20 file version

--- metagenlab_libs/chlamdb/chlamdb.py
+++ metagenlab_libs/chlamdb/chlamdb.py
@@ -383,8 +383,6 @@ def setup_cog(db, cog_dir):
 
     cog_fun_data = []
     for line_no, line in enumerate(fun_names_file):
-        if line_no==0:
-            continue
         function, random_str, description = line.split("\t") 
         cog_fun_data.append( (function.strip(), description.strip()) )
     db.load_cog_fun_data(cog_fun_data)
